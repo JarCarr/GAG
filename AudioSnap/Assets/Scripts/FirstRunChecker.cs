bbebc5f0da9aaa6dd416277c77fa5119a24fbef8
RosalynCrothers <40485766@live.napier.ac.uk>
2023-03-29 23:19:03 +0100

    Added registration
    Added registration, fixed camera in buggy screens not displaying buttons correctly

---
+++ AudioSnap/Assets/Scripts/FirstRunChecker.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FirstRunChecker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //int firstRun = PlayerPrefs.GetInt("savedFirstRun");
+        if (!PlayerPrefs.HasKey("FirstRun"))
+        {
+            PlayerPrefs.SetString("Name", "");
+            PlayerPrefs.SetInt("Score", 0);
+            SceneManager.LoadScene("Reg");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- AudioSnap/Assets/Scripts/SceneChange.cs
+++ AudioSnap/Assets/Scripts/SceneChange.cs
@@ -15,44 +15,14 @@ public class SceneChange : MonoBehaviour
     {
   
     }
-
-    public void goto_scan() {
-        changeRoom("Scan");
-        return;
-    }
-    public void goto_posters() {
-        changeRoom("Posters_main");
-        return;
-    }
-
-    public void goto_leaderboard() {
-        changeRoom("Leaderboard");
-        return;
-    }
-
-
-    public void goto_info() {
-        changeRoom("Info");
-        return;
-    }
-    public void goto_map() {
-        changeRoom("Map");
-        return;
-    }
-    public void goto_settings() {
-        changeRoom("Settings");
-        return;
-    }
     public void back()
     {
         changeRoom("Main_menu");
         return;
     }
 
-    void changeRoom(string roomname){
-        string lastrm = currentrm;
+    public void changeRoom(string roomname){
         SceneManager.LoadScene(roomname);
-        currentrm = roomname;
         return;
     }
 }
