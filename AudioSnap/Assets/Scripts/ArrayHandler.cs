22e111a1eff89e75ceb63bd70409ece33ae49ab2
RosalynCrothers <40485766@live.napier.ac.uk>
2023-04-05 21:32:11 +0100

    Added language options
    Added all languages, array stuff

---
+++ AudioSnap/Assets/Scripts/ArrayHandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Linq;
+
+
+public class ArrayHandler : MonoBehaviour
+{
+    int[] TextToNumArray(string input) {
+        int[] result;
+        List<int> tempResult = new List<int>();
+
+        string[] nums = input.Split(',');
+
+        Array.Reverse(nums);
+
+        foreach (string i in nums) {
+            tempResult.Add(Convert.ToInt32(i));
+        }
+
+        result = tempResult.ToArray();
+
+        return result;
+    }
+
+
+
+    string NumArrayToText(int[] array) {
+        string result = "";
+
+        Array.Reverse(array);
+
+        foreach (int i in array) {
+            result += Convert.ToString(i);
+        }
+
+        return result;
+    }
+}
--- AudioSnap/Assets/Scripts/Settings.cs
+++ AudioSnap/Assets/Scripts/Settings.cs
@@ -1,42 +1,95 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
+using UnityEngine.Localization;
+using UnityEngine.Localization.Settings;
+
 public class Settings : MonoBehaviour
 {
-    public string[] Languages = {"English", "French", "Chinese"};
+    //public string[] Languages = {"English", "French", "Chinese"};
 
-    public Text buttontext;
+    //public string language = PlayerPrefs.GetString("lang");
 
+    
     // Start is called before the first frame update
     void Start()
     {
-
     }
+
 
     public void nextlang() {
-        //get current language
-        //if language is x, set language to y
+ 
 
-        if (buttontext.text == "LANGUAGE:\nENGLISH")
+
+        
+        if (PlayerPrefs.GetString("lang") == "English")
         {
-            buttontext.text = "LANGUAGE:\nFRENCH";
-            //set language to french
+           PlayerPrefs.SetString("lang", "Spanish");
+           LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[1];
+           //sprite.sprite = spn;
+          
         }
-        else if (buttontext.text == "LANGUAGE:\nFRENCH")
+
+        else if (PlayerPrefs.GetString("lang") == "Spanish")
         {
-            buttontext.text = "LANGUAGE:\nCHINESE";
-            //set language to chinese
+            PlayerPrefs.SetString("lang", "Italian");
+            LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[2];
+            //sprite.sprite = ital;
+            
         }
-        else if (buttontext.text == "LANGUAGE:\nCHINESE")
+
+        else if (PlayerPrefs.GetString("lang") == "Italian")
         {
-            buttontext.text = "LANGUAGE:\nENGLISH";
-            //set language to english
+            PlayerPrefs.SetString("lang", "French");
+            LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[3];
+            //sprite.sprite = fr;
+            
         }
+
+        else if (PlayerPrefs.GetString("lang") == "French")
+        {
+            PlayerPrefs.SetString("lang", "German");
+            LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[4];
+           //sprite.sprite = ger;
+            
+        }
+
+        else if (PlayerPrefs.GetString("lang") == "German")
+        {
+            PlayerPrefs.SetString("lang", "Japanese");
+            LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[5];
+            //sprite.sprite = jpn;
+            
+        }
+                else if (PlayerPrefs.GetString("lang") == "Japanese")
+        {
+            PlayerPrefs.SetString("lang", "Portugese");
+            LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[6];
+            //sprite.sprite = jpn;
+            
+        }
+
+        else if (PlayerPrefs.GetString("lang") == "Portugese")
+        {
+            PlayerPrefs.SetString("lang", "English");
+            LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[0];
+            //sprite.sprite = eng;
+            
+        }
+        
 
     }
+
+    public void PrintLang() {
+        Debug.Log(PlayerPrefs.GetString("lang"));
+    }
+
+
+
+
 
 }
