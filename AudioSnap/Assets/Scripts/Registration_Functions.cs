a3630b88f51fa5b46eaf67d26727bb1afe4125b6
RosalynCrothers <40485766@live.napier.ac.uk>
2023-04-12 19:04:35 +0100

    added remaining posters, name validity checking

--- AudioSnap/Assets/Scripts/Registration_Functions.cs
+++ AudioSnap/Assets/Scripts/Registration_Functions.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.Localization;
+using UnityEngine.Localization.Settings;
 
 public class Registration_Functions : MonoBehaviour
 {
+
+    public Text errortext;
     //private string name;
     // Start is called before the first frame update
     void Start()
@@ -19,15 +23,25 @@ public class Registration_Functions : MonoBehaviour
         
     }
 
+    public bool ValidName(string name){
+        if (name == "" || name.Contains(" "))
+        {
+            return false;
+        }
+        return true;
+    }
+
     //Name Register Function for the Registration screen
     public void Register(InputField namebox) {
-        PlayerPrefs.SetString("name", namebox.text);
-        Debug.Log(PlayerPrefs.GetString("name"));
-        PlayerPrefs.SetInt("score", 0);
-        PlayerPrefs.SetString("values", "000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000");
-        PlayerPrefs.SetInt("FirstRun", 0);
-        PlayerPrefs.Save();
+        if(ValidName(namebox.text)) {
+            PlayerPrefs.SetString("name", namebox.text);
+            Debug.Log(PlayerPrefs.GetString("name"));
+            PlayerPrefs.SetInt("score", 0);
+            PlayerPrefs.SetString("values", "000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000");
+            PlayerPrefs.SetInt("FirstRun", 0);
+            PlayerPrefs.Save();
 
-        SceneManager.LoadScene("Main_menu");
+            SceneManager.LoadScene("Main_menu");
+        }
     }
 }
